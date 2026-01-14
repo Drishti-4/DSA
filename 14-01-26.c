0883f5166d3eb3bf57334f947593fbec9615e367
Drishti-4 <shrivastavadrishti6@gmail.com>
2026-01-14 22:54:24 +0530

    Add files via upload

---
+++ 14-01-26.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    int area;
+    printf("%d", area = 3.14 * n * n);
+    return 0;
+    
+
+}
