86c1091837e81e2456548d2ee79e57ab1fd13aab
Drishti-4 <shrivastavadrishti6@gmail.com>
2026-01-16 20:56:13 +0530

    Add files via upload

---
+++ problem.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+    int a,b,sum;
+
+    printf("enter two numbers:");
+    scanf("%d %d", &a,&b);
+
+    sum = a + b ;
+
+    printf("sum = %d\n" , sum);
+
+    return 0;
+
+
+}
