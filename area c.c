b3bd9162c5b96153847ec38ed58f2b982f28db36
Drishti-4 <shrivastavadrishti6@gmail.com>
2026-01-22 23:01:04 +0530

    Add files via upload

---
+++ area c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() { 
+    int radius;
+    printf("enter a number");
+    scanf("radius is %d\n",&radius);
+
+    printf("area is %d\n" , 3.14 * radius * radius);
+    return 0;
+
+}
---
+++ peri.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main () {
+    
+    
+    int a,b;
+    // a and b are sides of a rectangle
+    printf("enter a number");
+    scanf("%d", &a);
+
+    printf("enter a number");
+    scanf("%d", &b);
+
+    printf("perimeter is %d \n", 2 * (a + b));
+    return 0;
+
+}
